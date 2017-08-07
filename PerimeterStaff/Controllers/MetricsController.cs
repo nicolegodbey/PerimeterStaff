66707a1c9c8b41e0d38a93a9e4c43ef48f880f0d
nicolegodbey <nicoleg@perimeter.org>
2017-08-07 14:29:09 -0400

    added event counts page

--- PerimeterStaff/Controllers/MetricsController.cs
+++ PerimeterStaff/Controllers/MetricsController.cs
@@ -14,5 +14,46 @@ namespace PerimeterStaff.Controllers
         {
             return View();
         }
+
+        public List<Registered> eventsums = new List<Registered>();
+        public class Registered
+        {
+            public int eventID { get; set; }
+            public string eventTitle { get; set; }
+            public DateTime eventDate { get; set; }
+            public int registered { get; set; }
+            public int attended { get; set; }
+        }
+
+        public List<Registered> GetRegistered()
+        {
+            var api = PowerApi.CreateWebApiClient(); //.CreateWebApiClient();        
+
+            APIStoredProcRequest sp = new APIStoredProcRequest()
+            {
+                StoredProcedure = "api_12Stone_Custom_GetRegistrationCounts"
+
+            };
+            dynamic response = api.ExecuteStoredProcedure(sp);
+
+            foreach (var single in response[0])
+            {
+                Registered eventsum = new Registered();
+                eventsum.eventID = single.eventID;
+                eventsum.eventTitle = single.eventTitle;
+                //eventsum.eventDate = single.eventDate;
+                eventsum.registered = single.registered;
+                eventsum.attended = single.attended;
+                eventsums.Add(eventsum);
+            }
+
+            return eventsums;
+        }
+
+        public ActionResult EventCounts()
+        {
+            GetRegistered();
+            return View(eventsums);
+        }
     }
 }
